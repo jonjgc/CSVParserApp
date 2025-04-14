cadcee98ab7ad5df88ac499f5399068cbed0c607
Jonatas <jonatasmc04@gmail.com>
2025-04-14 10:53:30 -0300

    fix: desabilitar testes para verificação de erros no backend

--- public/api.php
+++ public/api.php
@@ -3,16 +3,17 @@
 header('Content-Type: application/json; charset=utf-8');
 
 // Habilitar erros apenas em desenvolvimento
-$isDevEnvironment = false;
-if ($isDevEnvironment) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', 0);
-    ini_set('display_startup_errors', 0);
-    error_reporting(0);
-}
+
+// $isDevEnvironment = false;
+// if ($isDevEnvironment) {
+//     ini_set('display_errors', 1);
+//     ini_set('display_startup_errors', 1);
+//     error_reporting(E_ALL);
+// } else {
+//     ini_set('display_errors', 0);
+//     ini_set('display_startup_errors', 0);
+//     error_reporting(0);
+// }
 
 // Carregar a classe
 require_once __DIR__ . '/../src/App/CsvProcessor.php';
@@ -53,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['csv_file'])) {
             'data' => $result['products']
         ]);
 
-        // Opcional: Remover o arquivo após processar
+        // essa parte do código é opcional: Remover o arquivo após processar
         unlink($uploadFile);
     } catch (Exception $e) {
         http_response_code(400);
